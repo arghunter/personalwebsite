8f9f5d15a890f7201d69a129968265865d10736e
ag <acgo8888@gmail.com>
2026-06-15 19:57:32 -0400

    better photos

--- .vitepress/theme/components/BlogImage.vue
+++ .vitepress/theme/components/BlogImage.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 defineProps<{
-    src: string
-    alt?: string
     caption?: string
 }>()
 </script>
 
 <template>
     <figure class="blog-figure">
-        <img :src="src" :alt="alt ?? caption ?? ''" class="blog-figure-img" />
+        <slot />
         <figcaption v-if="caption" class="blog-figure-caption">{{ caption }}</figcaption>
     </figure>
 </template>
